211a2236a7d455d8dffda21f85e801bae4d13f9a
exuanbo <exuanbo@protonmail.com>
2020-10-09 00:08:38 +0100

    chore(eslint): add config for next.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,18 @@ module.exports = {
     browser: true,
     es2021: true
   },
+  globals: {
+    React: 'readonly'
+  },
   extends: [
     'plugin:react/recommended',
     'standard'
   ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -19,5 +27,6 @@ module.exports = {
     '@typescript-eslint'
   ],
   rules: {
+    'react/react-in-jsx-scope': 'off'
   }
 }
