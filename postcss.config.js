5a7eb84292ec46576a6b11e2f882af6992a84485
exuanbo <exuanbo@protonmail.com>
2020-10-10 13:40:40 +0100

    build: conditionally transpile css

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-  plugins: ['tailwindcss', 'postcss-flexbugs-fixes', 'postcss-preset-env']
+  plugins:
+    process.env.NODE_ENV === 'production'
+      ? [
+        'tailwindcss',
+        'postcss-flexbugs-fixes',
+        [
+          'postcss-preset-env',
+          {
+            autoprefixer: {
+              flexbox: 'no-2009'
+            },
+            stage: 3,
+            features: {
+              'custom-properties': false
+            }
+          }
+        ]
+      ]
+      : [
+        'tailwindcss'
+      ]
 }
