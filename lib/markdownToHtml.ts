03f3669727e786873b218e364ed7dc79f977a365
exuanbo <exuanbo@protonmail.com>
2020-10-11 22:49:58 +0100

    chore(deps): add remark-external-links

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,8 +1,12 @@
 import remark from 'remark'
 import html from 'remark-html'
+import externalLinks from 'remark-external-links'
 
 const markdownToHtml = async (markdown: string): Promise<string> => {
-  const result = await remark().use(html).process(markdown)
+  const result = await remark()
+    .use(externalLinks, { rel: ['nofollow', 'noopener'] })
+    .use(html)
+    .process(markdown)
   return result.toString()
 }
 
