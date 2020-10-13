bb95f57daef985c6ef56c77d6a5ef788be3b63f4
exuanbo <exuanbo@protonmail.com>
2020-10-13 14:46:53 +0100

    chore: add script to rename github-markdown-css

---
+++ scripts/pre-compile.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+
+const renameToCssModule = modulePath => {
+  const filePath = generatFilePath(modulePath)
+  fs.copyFileSync(filePath, filePath.replace(/\.css$/, '.module.css'))
+
+  function generatFilePath(modulePath) {
+    return path.join(__dirname, '../', 'node_modules', modulePath)
+  }
+}
+
+renameToCssModule('github-markdown-css/github-markdown.css')
