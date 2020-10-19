6083feda2fb79b568c933732415bdec26d43025f
exuanbo <exuanbo@protonmail.com>
2020-10-19 23:56:12 +0100

    feat: `header.tsx`
    conditionally render `CoverImage`, `DateFormatter`

--- components/page/header.tsx
+++ components/page/header.tsx
@@ -6,8 +6,8 @@ import Author from '../../types/author'
 
 type Props = {
   title: string
-  coverImage: string
-  date: string
+  coverImage?: string
+  date?: string
   author: Author
 }
 
@@ -17,16 +17,20 @@ const PageHeader = ({ title, coverImage, date, author }: Props) => (
     <div className="hidden md:block md:mb-12">
       <Avatar name={author.name} picture={author.picture} />
     </div>
-    <div className="mb-8 md:mb-16 sm:mx-0">
-      <CoverImage title={title} src={coverImage} />
-    </div>
+    {coverImage ? (
+      <div className="mb-8 md:mb-16 sm:mx-0">
+        <CoverImage title={title} src={coverImage} />
+      </div>
+    ) : null}
     <div className="max-w-2xl mx-auto">
       <div className="block mb-6 md:hidden">
         <Avatar name={author.name} picture={author.picture} />
       </div>
-      <div className="mb-6 text-lg">
-        <DateFormatter dateString={date} />
-      </div>
+      {date ? (
+        <div className="mb-6 text-lg">
+          <DateFormatter dateString={date} />
+        </div>
+      ) : null}
     </div>
   </>
 )
