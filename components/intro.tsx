b776ae11b64dab8865b9ed7b6ffc32a180130038
exuanbo <exuanbo@protonmail.com>
2020-10-21 17:48:08 +0100

    refactor: rm unecessary tailwind customization

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,6 +1,6 @@
 const Intro: React.FunctionComponent = () => (
   <section className="flex flex-col items-center mt-16 mb-16 md:flex-row md:justify-between md:mb-12">
-    <h1 className="text-6xl font-bold leading-tight tracking-tighter md:text-8xl md:pr-8">
+    <h1 className="text-5xl font-bold leading-tight tracking-tighter md:text-6xl md:pr-8">
       UCCcn.
     </h1>
     <h4 className="mt-5 text-lg text-center md:text-left md:pl-8">
--- components/page/title.tsx
+++ components/page/title.tsx
@@ -3,7 +3,7 @@ type Props = {
 }
 
 const PageTitle: React.FunctionComponent<Props> = ({ children }: Props) => (
-  <h1 className="mb-12 text-6xl font-bold leading-tight tracking-tighter text-center md:text-7xl lg:text-8xl md:leading-none md:text-left">
+  <h1 className="mb-12 text-5xl font-bold leading-tight tracking-tighter text-center md:text-6xl lg:text-6xl md:leading-none md:text-left">
     {children}
   </h1>
 )
