c4962a94d0cc7c71b92b871b70237e4c152e658b
exuanbo <exuanbo@protonmail.com>
2020-10-22 15:12:08 +0100

    style: change padding of `Contariner`

--- components/container.tsx
+++ components/container.tsx
@@ -3,7 +3,7 @@ type Props = {
 }
 
 const Container: React.FunctionComponent<Props> = ({ children }: Props) => {
-  return <div className="container px-5 mx-auto">{children}</div>
+  return <div className="container px-4 mx-auto">{children}</div>
 }
 
 export default Container
